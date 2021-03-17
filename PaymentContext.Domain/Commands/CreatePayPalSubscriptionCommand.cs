33b4db5db045fd8e20bc7642aa2fdef814625f4f
Bruno Sales <brunosbernardes01@gmail.com>
2021-03-17 18:32:36 -0300

    create handler, iCommandResult and emailService

--- PaymentContext.Domain/Commands/CreatePayPalSubscriptionCommand.cs
+++ PaymentContext.Domain/Commands/CreatePayPalSubscriptionCommand.cs
@@ -1,9 +1,10 @@
 using System;
 using PaymentContext.Domain.Enums;
+using PaymentContext.Shared.Commands;
 
 namespace PaymentContext.Domain.Commands
 {
-    public class CreatePayPalSubscriptionCommand
+    public class CreatePayPalSubscriptionCommand : ICommand
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
@@ -26,5 +27,10 @@ namespace PaymentContext.Domain.Commands
         public string State { get; private set; }
         public string Country { get; private set; }
         public string ZipCode { get; private set; }
+
+        public void Validate()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
